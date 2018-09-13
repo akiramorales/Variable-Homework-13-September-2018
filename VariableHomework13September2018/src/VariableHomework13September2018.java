7b81269ed07d77f60e441197de7d306d379b9b4c
Akira Morales <akira.k.morales@gmail.com>
2018-09-13 13:38:20 -0400

    Add files via upload

---
+++ VariableHomework13September2018/src/VariableHomework13September2018.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @author Akira Morales
+ *Write a program which will display a person's name
+ *Write a program which will calculate the area of a rectangle
+ *Write a variable which will calculate the area and circumference of a circle
+ */
+public class VariableHomework13September2018
+{
+	public static void main(String[] args)
+	{
+		String name = "Akira";
+		System.out.print(name);
+		
+		int length = 10;
+		int width = 12;
+		double areaOfRectangle = length * width;
+		System.out.print(\n"The area of the rectangle is ");
+		System.out.print(areaOfRectangle);
+	}
+}
