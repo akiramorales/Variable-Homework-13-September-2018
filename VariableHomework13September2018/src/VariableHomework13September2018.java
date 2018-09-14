51eda6c4444ad45b40c3f606828103b9e4142c92
AKIRA <AKIRA@AKIRA-PC.fios-router.home>
2018-09-13 23:47:45 -0400

    Finished homework assignment.

--- VariableHomework13September2018/src/VariableHomework13September2018.java
+++ VariableHomework13September2018/src/VariableHomework13September2018.java
@@ -9,13 +9,23 @@ public class VariableHomework13September2018
 {
 	public static void main(String[] args)
 	{
-		String name = "Akira";
+		String name = "Akira\n";
+		System.out.print("The name is ");
 		System.out.print(name);
 		
 		int length = 10;
 		int width = 12;
 		double areaOfRectangle = length * width;
-		System.out.print(\n"The area of the rectangle is ");
+		System.out.print("The area of the rectangle is ");
 		System.out.print(areaOfRectangle);
+		
+		int radius = 3;
+		double pi = 3.14;
+		double area = radius * radius * pi;
+		System.out.print("\nThe area of the circle is ");
+		System.out.print(area);
+		double circumference = 2 * radius * pi;
+		System.out.print("\nThe circumference of the circle is ");
+		System.out.print(circumference);
 	}
 }
